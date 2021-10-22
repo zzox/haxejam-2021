e840d954d92bf6e94436123430959f503910ae4e
Tyler McCarthy <tweedlitweedli@gmail.com>
2021-10-22 12:26:45 -0500

    facing stuff

--- src/objects/Player.hx
+++ src/objects/Player.hx
@@ -16,6 +16,7 @@ class Player extends FlxSprite {
     static final VELOCITY = 60;
 
     public var facingDir:Dir = Down;
+    var facingDirs:Array<Dir> = [];
     var lrVel:Int = 0;
     var udVel:Int = 0;
     var holds:HoldsObj = {
@@ -50,6 +51,8 @@ class Player extends FlxSprite {
 
         velocity.set(lrVel * VELOCITY, udVel * VELOCITY);
 
+        trace('facing: $facingDir');
+
         super.update(elapsed);
     }
 
@@ -67,7 +70,6 @@ class Player extends FlxSprite {
 
             if (holds.swing != 0) {
                 justReleased = true;
-                trace('justReleased', sword.state);
             }
 
             holds.swing = 0;
@@ -96,24 +98,30 @@ class Player extends FlxSprite {
         if (controlsPressed.left) {
             // if we just released the sword, or we just pressed a key
             if (justReleased || (holds.left == 0 && canTurn)) {
-                facingDir = Left;
+                facingDirs.contains(Left) ? null : facingDirs.unshift(Left);
             }
 
             lrVel = -1;
             holds.left += elapsed;
         } else {
             holds.left = 0;
+            if (facingDirs.contains(Left)) {
+                facingDirs.remove(Left);
+            }
         }
 
         if (controlsPressed.right) {
             if (justReleased || (holds.right == 0 && canTurn)) {
-                facingDir = Right;
+                facingDirs.contains(Right) ? null : facingDirs.unshift(Right);
             }
 
             lrVel = 1;
             holds.right += elapsed;
         } else {
             holds.right = 0;
+            if (facingDirs.contains(Right)) {
+                facingDirs.remove(Right);
+            }
         }
 
         if (controlsPressed.left && controlsPressed.right) {
@@ -126,24 +134,30 @@ class Player extends FlxSprite {
 
         if (controlsPressed.up) {
             if (justReleased || (holds.up == 0 && canTurn)) {
-                facingDir = Up;
+                facingDirs.contains(Up) ? null : facingDirs.unshift(Up);
             }
 
             udVel = -1;
             holds.up += elapsed;
         } else {
             holds.up = 0;
+            if (facingDirs.contains(Up)) {
+                facingDirs.remove(Up);
+            }
         }
 
         if (controlsPressed.down) {
             if (justReleased || (holds.down == 0 && canTurn)) {
-                facingDir = Down;
+                facingDirs.contains(Down) ? null : facingDirs.unshift(Down);
             }
 
             udVel = 1;
             holds.down += elapsed;
         } else {
             holds.down = 0;
+            if (facingDirs.contains(Down)) {
+                facingDirs.remove(Down);
+            }
         }
 
         if (controlsPressed.up && controlsPressed.down) {
@@ -153,5 +167,7 @@ class Player extends FlxSprite {
                 udVel = 1;
             }
         }
+
+        facingDir = facingDirs[0] != null ? facingDirs[0] : facingDir;
     }
 }
